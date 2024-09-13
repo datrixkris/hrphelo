5498646bc2f3ea511cc9f8476ccaed20abf72877
Christian Amoakohene <camoakohene84@gmail.com>
2024-09-13 14:02:51 +0000

    background and colors

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="">
       <button className="button">hello</button>
+      <div className="bg-base-100 size-[500px] border border-base-300"></div>
     </div>
   );
 }
