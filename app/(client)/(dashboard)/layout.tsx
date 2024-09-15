63836cdc79b977ef9cb2fc71b70ee0c3222b9190
Christian Amoakohene <camoakohene84@gmail.com>
2024-09-15 21:26:49 +0000

    client side dashboard

---
+++ app/(client)/(dashboard)/layout.tsx
@@ -0,0 +1,19 @@
+import Sidebar from "../components/Sidebar";
+import Topnav from "../components/Topnav";
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <div className="fixed top-0 bottom-0 w-[250px]">
+        <Sidebar />
+      </div>
+      <div className="ml-[250px] primary-bg min-h-screen">
+        <div>
+          <Topnav />
+
+          <div className="maximum-width py-5">{children}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/(client)/components/Sidebar.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import React from "react";
+import ActiveLink from "../../components/ActiveLink";
+import { Icon } from "@iconify/react";
+
+const Sidebar = () => {
+  return (
+    <div className="bg-base-100 h-full p-4 border-r border-base-300">
+      {/* logo  */}
+      <div className="">
+        <h1 className="text-cyan-500 font-bold text-xl text-center">
+          company logo
+        </h1>
+      </div>
+      {/* title  */}
+      <div className="mt-4 border-t border-base-300">
+        <div className="my-7 space-y-3 ">
+          {/* dashboard */}
+          <div className="">
+            <ActiveLink href="/">
+              <div className="flex gap-2 items-center">
+                <Icon icon="hugeicons:dashboard-square-03" />
+                <span>Dashboard</span>
+              </div>
+            </ActiveLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
