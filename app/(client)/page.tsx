7d8912f6fe968c8ad5095e2cfb3be1e35935f723
Christian Amoakohene <camoakohene84@gmail.com>
2024-09-15 04:44:15 +0000

    added Icons pack from iconify

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,8 +1,13 @@
+import { Icon } from "@iconify/react";
+
 export default function Home() {
   return (
     <div className="">
       <button className="button">hello</button>
-      <div className="bg-base-100 size-[500px] border border-base-300"></div>
+      <div className="bg-base-100 size-[500px] border border-base-300">
+        <Icon icon="hugeicons:account-setting-03" className="text-3xl" />
+        sup
+      </div>
     </div>
   );
 }
