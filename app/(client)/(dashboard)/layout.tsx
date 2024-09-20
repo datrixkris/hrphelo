f17e0ba5b00b7792fa6910d202d86be2e78822ed
latic1 <laticlatif2@gmail.com>
2024-09-20 13:19:24 +0000

    auth

--- app/(client)/(dashboard)/layout.tsx
+++ app/(client)/(dashboard)/layout.tsx
@@ -1,16 +1,30 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Sidebar from "../components/Sidebar";
 import Topnav from "../components/Topnav";
+import { useAuthStore } from "@/app/stores/auth-store";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = useAuthStore.getState().accessToken;
+
+    if (!token) {
+      router.push("/auth/login");
+    }
+  }, [router]); 
+
   return (
     <div>
-      <div className="fixed top-0 bottom-0 w-[250px]">
+      <div className="fixed bottom-0 top-0 w-[250px]">
         <Sidebar />
       </div>
-      <div className="ml-[250px] primary-bg min-h-screen">
+      <div className="primary-bg ml-[250px] min-h-screen">
         <div>
           <Topnav />
-
           <div className="maximum-width py-5">{children}</div>
         </div>
       </div>
--- app/stores/auth-store.ts
+++ app/stores/auth-store.ts
@@ -1,31 +1,46 @@
-import { create } from "zustand"
+import { create } from "zustand";
 import { User } from "../types/user-types";
 import { api } from "../axiosApi/api";
 
 interface AuthStore {
-    accessToken: string;
+    accessToken: string | null;
     user: User | null;
-    fetchUserData: () => Promise<void>
+    fetchUserData: () => Promise<void>;
+    resetData: {
+        password: string;
+        confirmPassword: string;
+        otp: string;
+    } | null;
 }
 
 // Helper function to load data from localStorage
 const getLocalStorage = (key: string) => {
-    const storedValue = localStorage.getItem(key);
-    return storedValue ? JSON.parse(storedValue) : null;
+    if (typeof window !== "undefined") {
+        const storedValue = localStorage.getItem(key);
+        return storedValue ? JSON.parse(storedValue) : null;
+    }
+    return null;
 };
 
 export const useAuthStore = create<AuthStore>((set) => ({
-    // state variables
+    // State variables
     accessToken: getLocalStorage('accessToken'),
     user: null,
 
     // Actions
-    fetchUserData: async() => {
+    fetchUserData: async () => {
         try {
-            const response = await api.get('/v1/user')
-            set({user: response.data})
+            const response = await api.get('/v1/user');
+            set({ user: response.data });
         } catch (err) {
-            console.error(err)
+            console.error('Error fetching user data:', err);
         }
+    },
+
+    // Initialize resetData
+    resetData: {
+        password: '',
+        confirmPassword: '',
+        otp: ''
     }
-}))
+}));
