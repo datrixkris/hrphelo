34bb9453dcd9febc4053db8f1aabc7d45289dc74
latic1 <laticlatif2@gmail.com>
2024-11-08 09:40:36 +0000

    first commit

--- app/(client)/(dashboard)/projects/[projectId]/[projectSlug]/page.tsx
+++ app/(client)/(dashboard)/projects/[projectId]/[projectSlug]/page.tsx
@@ -5,38 +5,20 @@ import PageTitleWithCrumbs from "@/app/components/PageTitleWithCrumbs";
 import { useEffect, useState } from "react";
 import ProjectDetails from "../../components/project-details/ProjectDetails";
 import Taskboard from "../../components/taskboard/Taskboard";
-import { useProjectStore } from "../../stores/project-store";
 import { useKanbanStore } from "../../kanbanStore";
 import { useParams } from "next/navigation";
 
 const Project = () => {
-  const params = useParams<{ projectSlug: string }>();
+  const params = useParams<{ projectId: string; projectSlug: string }>();
+
   const [toggleTaskboard, setToggleTaskboard] = useState(true);
-  const { projects, fetchProjects } = useProjectStore();
 
   const { fetchColumn } = useKanbanStore();
 
   useEffect(() => {
-    if (!params.projectSlug) return; // Return early if projectSlug is not defined
-
-    async function loadProjectsAndFetchColumn() {
-      await fetchProjects(); // Ensure projects are fetched
-    }
-
-    loadProjectsAndFetchColumn();
-  }, [params.projectSlug, fetchProjects]);
-
-  useEffect(() => {
-    if (projects.length === 0 || !params.projectSlug) return; // Wait until projects are populated
-
-    const project = projects.find((proj) => proj.slug === params.projectSlug);
-
-    if (project?.id) {
-      fetchColumn(project.id);
-    } else {
-      console.log("Project not found");
-    }
-  }, [projects, params.projectSlug, fetchColumn]);
+    if (!params.projectId) return;
+    fetchColumn(Number(params.projectId));
+  }, [params.projectId, fetchColumn]);
 
   return (
     <div>
