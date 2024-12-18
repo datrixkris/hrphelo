195d234b38421744a058a99bf370e9021ec40aa3
Christian Amoakohene <camoakohene@datrixtechsolutions.com>
2024-12-18 04:13:37 +0000

    minor refactors and changes

--- app/(client)/(dashboard)/projects/components/ProjectCard.tsx
+++ app/(client)/(dashboard)/projects/components/ProjectCard.tsx
@@ -5,69 +5,75 @@ import ProgressBar, { progressColor } from "./ProgressBar";
 
 const ProjectCard = ({ project }: { project: ProjectData }) => {
   return (
-    <div className="h-full space-y-5 rounded bg-base-100 p-5 text-sm font-medium text-neutral-500 shadow">
-      {/* title and stats */}
-      <div className="">
-        <Link href={`/projects/${project?.id}/${project?.slug}/`}>
-          <h2 className="mb-2 text-lg font-semibold capitalize text-base-content transition-colors hover:text-hr-yellow">
-            {project.name}
-          </h2>
-        </Link>
-        <p className="text-xs">
-          <span className="text-base-content">1</span> open task,{" "}
-          <span className="text-base-content">9</span> tasks completed
-        </p>
-      </div>
-      {/* descriptoin */}
-      <div className="h-20">
-        <p className="line-clamp-4">{project.description}</p>
-      </div>
-      {/* deadline */}
-      <div className="">
-        <p className="mb-1 font-semibold text-base-content">Deadline:</p>
-        <p className="">{project.end_date}</p>
-      </div>
-      {/* Project lead */}
-      <div className="">
-        <p className="mb-1 font-semibold text-base-content">Project Lead:</p>
-        <div className="avatar">
-          <div className="w-10 rounded-full">
-            <img
-              src={project.projectLead?.image}
-              alt={project.projectLead?.name}
-            />
+    <Link href={`/projects/${project?.id}/${project?.slug}/`}>
+      <div className="flex h-full flex-col justify-between gap-3 rounded bg-base-100 p-5 text-sm font-medium text-neutral-500 shadow transition-transform hover:scale-[103%]">
+        <div className="space-y-3">
+          {/* title and stats */}
+          <div className="">
+            <h2 className="mb-2 text-lg font-semibold capitalize text-base-content transition-colors">
+              {project.name}
+            </h2>
+
+            <p className="text-xs">
+              <span className="text-base-content">1</span> open task,{" "}
+              <span className="text-base-content">9</span> tasks completed
+            </p>
+          </div>
+          {/* descriptoin */}
+          <div className="h-16">
+            <p className="line-clamp-3">{project.description}</p>
+          </div>
+          {/* deadline */}
+          <div className="">
+            <p className="mb-1 font-semibold text-base-content">Deadline:</p>
+            <p className="">{project.end_date}</p>
+          </div>
+          {/* Project lead */}
+          <div className="">
+            <p className="mb-1 font-semibold text-base-content">
+              Project Lead:
+            </p>
+            <div className="avatar">
+              <div className="w-12 rounded-full">
+                <img
+                  src={project.projectLead?.image}
+                  alt={project.projectLead?.name}
+                />
+              </div>
+            </div>
+            <p className="text-xs">{project.projectLead?.name}</p>
+          </div>
+          {/* team  */}
+          <div className="">
+            <p className="mb-1 font-semibold text-base-content">Team:</p>
+            <div className="avatar-group -space-x-6 rtl:space-x-reverse">
+              {project.members?.map((member) => {
+                return (
+                  <div className="avatar" key={member.id}>
+                    <div className="w-10">
+                      <img src={member.staff.image} alt={member.staff.name} />
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
-      </div>
-      {/* team  */}
-      <div className="">
-        <p className="mb-1 font-semibold text-base-content">Team:</p>
-        <div className="avatar-group -space-x-6 rtl:space-x-reverse">
-          {project.members?.map((member) => {
-            return (
-              <div className="avatar" key={member.id}>
-                <div className="w-10">
-                  <img src={member.staff.image} alt={member.staff.name} />
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
 
-      {/* progress */}
-      <div className="">
-        <div className="mb-3 flex justify-between">
-          <p className="font-semibold text-base-content">Progress:</p>
-          <p
-            className={`font-bold ${"text-" + progressColor(project.progress)}`}
-          >
-            {project.progress}%
-          </p>
+        {/* progress */}
+        <div className="">
+          <div className="mb-3 flex justify-between">
+            <p className="font-semibold text-base-content">Progress:</p>
+            <p
+              className={`font-bold ${"text-" + progressColor(project.progress)}`}
+            >
+              {project.progress}%
+            </p>
+          </div>
+          <ProgressBar progress={project.progress} />
         </div>
-        <ProgressBar progress={project.progress} />
       </div>
-    </div>
+    </Link>
   );
 };
 
