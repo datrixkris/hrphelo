75e5cf0ea0045b00e59f3bee6385a0474e61d0c9
Christian Amoakohene <camoakohene@datrixtechsolutions.com>
2025-03-03 11:43:12 +0000

    undefined to null

--- app/components/StaffOnboardingProgress.tsx
+++ app/components/StaffOnboardingProgress.tsx
@@ -255,7 +255,7 @@ export const ChecklistItem = (data: TOnboarding) => {
               icon="hugeicons:checkmark-badge-03"
               className="h-4 w-4 rounded text-success"
             />
-            {data.staffChecklists[0].checklistQueries === undefined && (
+            {data.staffChecklists[0].checklistQueries === null && (
               <button
                 onClick={() => setIsQueryFormOpen(!isQueryFormOpen)}
                 className="btn btn-ghost btn-xs"
