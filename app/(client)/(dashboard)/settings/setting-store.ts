a199bda62c5f9610c437ec5441174a8b9afd6308
Christian Amoakohene <camoakohene@datrixtechsolutions.com>
2025-05-16 05:46:48 +0000

    build error resolution

--- app/(client)/(dashboard)/settings/setting-store.ts
+++ app/(client)/(dashboard)/settings/setting-store.ts
@@ -4,82 +4,75 @@ import { api } from "@/app/axiosApi/api";
 import { AxiosError } from "axios";
 import { toast } from "react-toastify";
 
-
 export interface EditCompanyDetail {
-    name: string;
-    address: string;
-    contact_person?: string;
-    contact_person_contact?: string;
-    contact: string;
-    email: string;
-    company_size: string;
-    company_light_theme_logo:string;
-    company_dark_theme_logo:string;
+  name: string;
+  address: string;
+  contact_person?: string;
+  contact_person_contact?: string;
+  contact: string;
+  email: string;
+  company_size: string;
+  company_light_theme_logo: string;
+  company_dark_theme_logo: string;
 }
 
-
-
 interface SettingsStore {
-    company: Company | null;
-    loading?: boolean;
-    error?: string | null;
-    updatingData?: boolean;
+  company: Company | null;
+  loading?: boolean;
+  error?: string | null;
+  updatingData?: boolean;
 
-    fetchCompany: () => Promise<void>;
-    updateCompanyDetails: (data: EditCompanyDetail) => Promise<void>;
-
+  fetchCompany: () => Promise<void>;
+  updateCompanyDetails: (data: EditCompanyDetail) => Promise<void>;
 }
 
 interface ApiErrorResponse {
-    message?: string;
-    code?: number;
-    error?: string;
+  message?: string;
+  code?: number;
+  error?: string;
 }
 
 export const useSettingsStore = create<SettingsStore>((set, get) => ({
-    company: null,
-    loading: false,
-    updatingData: false,
+  company: null,
+  loading: false,
+  updatingData: false,
 
+  fetchCompany: async () => {
+    set({ loading: true, error: null });
 
-    fetchCompany: async () => {
-        set({ loading: true, error: null });
+    try {
+      const response = (await api.get("/v1/company")).data;
+      set(() => ({ company: response, loading: false }));
+    } catch (err) {
+      const axiosError = err as AxiosError<ApiErrorResponse>;
+      set(() => ({
+        error:
+          axiosError?.response?.data.error ??
+          axiosError?.response?.data.message ??
+          axiosError.message,
+        loading: false,
+      }));
+      toast.error(get().error);
+      console.error(err);
+    }
+  },
 
-        try {
-            const response = (await api.get("/v1/company")).data;
-            set(() => ({ company: response, loading: false }));
-        } catch (err) {
-            const axiosError = err as AxiosError<ApiErrorResponse>;
-            set(() => ({
-                error:
-                    axiosError?.response?.data.error ??
-                    axiosError?.response?.data.message ??
-                    axiosError.message,
-                loading: false,
-            }));
-            toast.error(get().error);
-            console.error(err);
-        }
-    },
+  updateCompanyDetails: async (data) => {
+    set({ updatingData: true, error: null });
 
-    updateCompanyDetails: async (data) => {
-        set({ updatingData: true, error: null });
-
-        try {
-            const response = await api.put(`/v1/company`, data);
-            set(() => ({ updatingData: false }));
-        } catch (err) {
-            const axiosError = err as AxiosError<ApiErrorResponse>;
-            set(() => ({
-                error:
-                    axiosError?.response?.data.error ??
-                    axiosError?.response?.data.message ??
-                    axiosError.message,
-                updatingData: false,
-            }));
-
-        }
-    },
-
-
-}))
+    try {
+      const response = await api.put(`/v1/company`, data);
+      set(() => ({ updatingData: false }));
+      console.log(response);
+    } catch (err) {
+      const axiosError = err as AxiosError<ApiErrorResponse>;
+      set(() => ({
+        error:
+          axiosError?.response?.data.error ??
+          axiosError?.response?.data.message ??
+          axiosError.message,
+        updatingData: false,
+      }));
+    }
+  },
+}));
