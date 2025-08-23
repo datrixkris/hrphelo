1d511becf149dfe8de2fc5c2c608731c3f9d8681
Christian Amoakohene <camoakohene@datrixtechsolutions.com>
2025-08-23 12:23:02 +0000

    lint errors

--- app/(client)/(dashboard)/(users)/users-accounts/components/UserPermissions.tsx
+++ app/(client)/(dashboard)/(users)/users-accounts/components/UserPermissions.tsx
@@ -1,60 +1,60 @@
-import React, { useState } from "react";
-import Button from "@/app/components/Button";
-import { Permissions, UserModules } from "../types";
-// import PermissionsComponent from "./Permissions";
-import { useUserAccountStore } from "../user-account-store";
+// import React, { useState } from "react";
+// import Button from "@/app/components/Button";
+// import { Permissions, UserModules } from "../types";
+// // import PermissionsComponent from "./Permissions";
+// import { useUserAccountStore } from "../user-account-store";
 
-const UserPermissions = ({
-  onPermissionsSubmit,
-  userPermissions,
-}: {
-  onPermissionsSubmit: (permissions: UserModules[]) => void;
-  userPermissions: Permissions[] | null;
-}) => {
-  const [permissions, setPermissions] = useState<UserModules[] | null>(null);
-  const updatingData = useUserAccountStore((state) => state.updatingData);
+// const UserPermissions = ({
+//   onPermissionsSubmit,
+//   userPermissions,
+// }: {
+//   onPermissionsSubmit: (permissions: UserModules[]) => void;
+//   userPermissions: Permissions[] | null;
+// }) => {
+//   const [permissions, setPermissions] = useState<UserModules[] | null>(null);
+//   const updatingData = useUserAccountStore((state) => state.updatingData);
 
-  const handleSubmit = () => {
-    // send data to parent component
-    if (permissions) {
-      onPermissionsSubmit(permissions);
-    }
-  };
+//   const handleSubmit = () => {
+//     // send data to parent component
+//     if (permissions) {
+//       onPermissionsSubmit(permissions);
+//     }
+//   };
 
-  // get permissions module data from the permissions component anytime a user checks a permission box
-  // const getUserPermissions = (data: UserModules[]) => {
-  //   console.log(data);
-  //   setPermissions(data);
-  // };
+//   // get permissions module data from the permissions component anytime a user checks a permission box
+//   // const getUserPermissions = (data: UserModules[]) => {
+//   //   console.log(data);
+//   //   setPermissions(data);
+//   // };
 
-  return (
-    <div>
-      {/* <PermissionsComponent
-        userPermissions={userPermissions}
-        getUserPermissions={getUserPermissions}
-      /> */}
+//   return (
+//     <div>
+//       {/* <PermissionsComponent
+//         userPermissions={userPermissions}
+//         getUserPermissions={getUserPermissions}
+//       /> */}
 
-      <div className="!mt-10">
-        {userPermissions ? (
-          <Button
-            className="mx-auto w-1/2"
-            disabled={updatingData}
-            onClick={() => handleSubmit()}
-          >
-            {updatingData ? "Editing permissions..." : "Edit permissions"}
-          </Button>
-        ) : (
-          <Button
-            onClick={() => handleSubmit()}
-            className="mx-auto w-1/2"
-            disabled={updatingData}
-          >
-            {updatingData ? "Creating user..." : "Create user"}
-          </Button>
-        )}
-      </div>
-    </div>
-  );
-};
+//       <div className="!mt-10">
+//         {userPermissions ? (
+//           <Button
+//             className="mx-auto w-1/2"
+//             disabled={updatingData}
+//             onClick={() => handleSubmit()}
+//           >
+//             {updatingData ? "Editing permissions..." : "Edit permissions"}
+//           </Button>
+//         ) : (
+//           <Button
+//             onClick={() => handleSubmit()}
+//             className="mx-auto w-1/2"
+//             disabled={updatingData}
+//           >
+//             {updatingData ? "Creating user..." : "Create user"}
+//           </Button>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
 
-export default UserPermissions;
+// export default UserPermissions;
